4877a12f1f5ba80ad867ba704c5d91a9a8db3188
noel-pena <azulu88@hotmail.com>
2025-06-01 19:00:43 -0400

    continued porfolio

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardMedia, Grid } from "@mui/material";
+import { Box, Card, CardMedia, Grid, Stack, Typography } from "@mui/material";
 
 function Hero() {
 	return (
@@ -16,21 +16,69 @@ function Hero() {
 				alignItems="center"
 				spacing={4}
 			>
-				<Grid size={{ xs: 12, md: 6 }}>
+				<Grid size={{ xs: 12, md: 6 }} minWidth="350px">
 					<Box
 						sx={{
 							background: "linear-gradient(135deg, #2c5364, #203a43, #0f2027)",
-							borderRadius: 3,
-							padding: 2,
+							borderRadius: 2,
+							pb: 2,
+							px: 2,
 							boxShadow: 6,
 						}}
 					>
+						<Box
+							sx={{
+								display: "flex",
+								flexDirection: "row",
+								justifyContent: "space-between",
+							}}
+						>
+							<Stack
+								sx={{
+									py: 2,
+									display: "flex",
+									gap: "8px",
+									flexDirection: "row",
+								}}
+							>
+								<Box
+									sx={{
+										width: "12px",
+										height: "12px",
+										borderRadius: "50%",
+										bgcolor: "#FF5757",
+									}}
+								/>
+								<Box
+									sx={{
+										width: "12px",
+										height: "12px",
+										borderRadius: "50%",
+										bgcolor: "#FFDA65",
+									}}
+								/>
+								<Box
+									sx={{
+										width: "12px",
+										height: "12px",
+										borderRadius: "50%",
+										bgcolor: "#4CAF50",
+									}}
+								/>
+							</Stack>
+							<Typography
+								variant="subtitle2"
+								sx={{ display: "flex", alignItems: "center" }}
+							>
+								portfolio/components/about-me.tsx
+							</Typography>
+						</Box>
 						<Card
 							sx={{
-								backgroundColor: "#1e1e1e",
+								bgcolor: "#1e1e1e",
 								color: "#d4d4d4",
 								fontFamily: "monospace",
-								padding: 2,
+								p: 1,
 								minHeight: 200,
 							}}
 						>

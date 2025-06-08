78ac9603a11b23f0bb077a21fcbf3098a60000dd
noel-pena <azulu88@hotmail.com>
2025-06-08 14:09:39 -0400

    added content for dev box

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,23 @@
 import { Box, Card, CardMedia, Grid, Stack, Typography } from "@mui/material";
 
 function Hero() {
+	const codeLines = [
+		{ id: 1, content: "type Developer = {" },
+		{ id: 2, content: "  name: string" },
+		{ id: 3, content: "  skills: Array<string>" },
+		{ id: 4, content: "  experience: 'string'" },
+		{ id: 5, content: "}" },
+		{ id: 6, content: "" },
+		{ id: 7, content: "const developer: Developer = {" },
+		{ id: 8, content: "  name: 'Noel Pena'" },
+		{
+			id: 9,
+			content: "  skills: ['TypeScript', 'JavaScript', 'Flutter', 'Kotlin']",
+		},
+		{ id: 10, content: "  experience: 'Full Stack Web Developer'" },
+		{ id: 11, content: "}" },
+	];
+
 	return (
 		<Box
 			sx={{
@@ -16,7 +33,7 @@ function Hero() {
 				alignItems="center"
 				spacing={4}
 			>
-				<Grid size={{ xs: 12, md: 6 }} minWidth="350px">
+				<Grid size={{ xs: 12, md: 6 }} minWidth="450px">
 					<Box
 						sx={{
 							background: "linear-gradient(135deg, #2c5364, #203a43, #0f2027)",
@@ -82,7 +99,33 @@ function Hero() {
 								minHeight: 200,
 							}}
 						>
-							<CardMedia>left</CardMedia>
+							<CardMedia sx={{ p: 1 }}>
+								<Typography
+									variant="subtitle2"
+									sx={{ color: "darkgrey", py: 1 }}
+								>
+									portfolio/components/about-me.tsx
+								</Typography>
+								{codeLines.map((line) => (
+									<Box px={1} key={line.id}>
+										<Stack display="flex" direction="row">
+											<Typography
+												sx={{
+													color: "gray",
+													pr: 1,
+													minWidth: "28px",
+													textAlign: "right",
+												}}
+											>
+												{line.id}
+											</Typography>
+											<Typography sx={{ color: "gray", whiteSpace: "pre" }}>
+												{line.content}
+											</Typography>
+										</Stack>
+									</Box>
+								))}
+							</CardMedia>
 						</Card>
 					</Box>
 				</Grid>

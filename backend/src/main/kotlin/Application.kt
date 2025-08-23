c80f449565f915f94d6bfc2e5429070cf51b4d02
noel-pena <azulu88@hotmail.com>
2025-08-23 17:06:41 -0400

    creating backend for an email service to replace the current one

---
+++ backend/src/main/kotlin/Application.kt
@@ -0,0 +1,11 @@
+package noelpena
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}

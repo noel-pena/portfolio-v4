59f8addbeb68120653db2eb55618d4a9aa3c3255
noel-pena <azulu88@hotmail.com>
2025-10-28 17:26:05 -0400

    added more to the test

--- src/app/components/Projects/Projects.test.tsx
+++ src/app/components/Projects/Projects.test.tsx
@@ -12,5 +12,40 @@ describe("Projects section", () => {
 		setup();
 
 		expect(screen.getByText("Featured")).toBeInTheDocument();
+		expect(screen.getByText("Projects")).toBeInTheDocument();
+
+		expect(
+			screen.getByText(
+				"A Showcase of My Personal Projects as a Full-stack Developer",
+			),
+		).toBeInTheDocument();
+
+		expect(screen.getByText("Portfolio")).toBeInTheDocument();
+		expect(
+			screen.getByText(
+				"A clean personal portfolio showcasing featured work, skills, and contact—all responsive and built with modern design principles.",
+			),
+		).toBeInTheDocument();
+
+		expect(screen.getByText("Interviewer AI")).toBeInTheDocument();
+		expect(
+			screen.getByText(
+				"An AI-powered web tool designed to simulate job interviews and generate questions using OpenAI for mock interview practice.",
+			),
+		).toBeInTheDocument();
+
+		expect(screen.getByText("Wallpaper App")).toBeInTheDocument();
+		expect(
+			screen.getByText(
+				"A mobile app offering curated high-quality wallpapers with tagging, downloads, and favorites, built with Flutter and Kotlin.",
+			),
+		).toBeInTheDocument();
+
+		expect(screen.getByText("Code-Assisting Discord Bot")).toBeInTheDocument();
+		expect(
+			screen.getByText(
+				"A smart Discord bot for code help, using OpenAI to debug, explain, and generate code directly inside your development servers.",
+			),
+		).toBeInTheDocument();
 	});
 });

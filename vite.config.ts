d57c5e4f8c216db333c20fd31a54a7a17bd55be5
noel-pena <azulu88@hotmail.com>
2026-03-02 22:59:02 -0500

    Making changes to yaml, fixing some build errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineViteConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 
-export default defineConfig({
+const viteConfig = defineViteConfig({
 	plugins: [react()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes("node_modules")) {
+						return id
+							.toString()
+							.split("node_modules/")[1]
+							.split("/")[0]
+							.toString();
+					}
+				},
+			},
+		},
+	},
+});
+
+const vitestConfig = defineVitestConfig({
 	test: {
 		globals: true,
 		environment: "jsdom",
@@ -10,3 +29,5 @@ export default defineConfig({
 		css: true,
 	},
 });
+
+export default mergeConfig(viteConfig, vitestConfig);
